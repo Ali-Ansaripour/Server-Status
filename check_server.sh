41ed9529b1f0f8e7d0d75a524c895c5eb86e5130
Ali <98914578+Ali-Ansaripour@users.noreply.github.com>
2024-07-30 10:08:32 +0330

    Update check_server.sh

--- check_server.sh
+++ check_server.sh
@@ -7,14 +7,25 @@ SMS_API_URL="https://...."
 API_KEY="youre sms api key"
 LOG_FILE="/path/to/log_file.log"
 
-#Func to send sms
+#Func to send sms --- Via POST request 
+# send_sms() {
+#  local message=$1
+#  curl -X POST "$SMS_API_URL " \ -H "Content-Type: application/Json" 
+#    \ -d "{\"apikey\": \"$API_KEY\", 
+#    \"number\": \"$PHONE_NUMBER\",
+#    \"message\": \"$message\" } "
+#}
+
+#Func to send sms --- Via GET 
 send_sms() {
   local message=$1
-  curl -X POST "$SMS_API_URL " \ -H "Content-Type: application/Json" 
-    \ -d "{\"apikey\": \"$API_KEY\", 
-    \"number\": \"$PHONE_NUMBER\",
-    \"message\": \"$message\" } "
-}
+  local encoded_message=$(echo "$message" | jq -sRr @uri)
+  curl -G "$SMS_API_URL " \
+    --data-urlencode "apiKey=$API_KEY"\
+    --data-urlencode "number=$PHONE_NUMBER"\
+    --data-urlencode "message=$encoded_message"
+  }
+  
 
 #ping server
 ping -c 1 $SERVER_IP  &> /dev/null
